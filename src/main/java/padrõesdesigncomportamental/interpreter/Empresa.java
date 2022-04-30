896731991a30dea467cea738a8545521f8be8285
Matheus Telles <mwtelles2012@hotmail.com>
2022-04-30 10:41:00 -0300

    Interpreter

---
+++ src/main/java/padrõesdesigncomportamental/interpreter/Empresa.java
@@ -0,0 +1,12 @@
+public class Empresa {
+
+    public static String formula = "nota1 * 2 + nota2";
+
+    public static double calcularFluxoCaixa(double nota1, double nota2) {
+        String expressao;
+        expressao = formula.replace("nota1", Double.toString(nota1));
+        expressao = expressao.replace("nota2", Double.toString(nota2));
+        InterpretadorExpressao interpretador = new InterpretadorExpressoesAritmeticas(expressao);
+        return interpretador.interpretar();
+    }
+}
